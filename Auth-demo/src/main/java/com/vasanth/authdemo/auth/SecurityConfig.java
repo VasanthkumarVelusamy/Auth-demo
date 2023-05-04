4f427430c66784cd396c903e4d724dde5e70e827
Vasanth Kumar <vasanthkumar.work@gmail.com>
2023-05-04 23:20:33 +0530

    Authentication filter was added to integrate filter for JWT

---
+++ Auth-demo/src/main/java/com/vasanth/authdemo/auth/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.vasanth.authdemo.user.auth;
+
+import com.vasanth.authdemo.JwtAuthenticationFilter;
+import com.vasanth.authdemo.JwtAuthenticationManager;
+import com.vasanth.authdemo.user.UsersService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtService jwtService, UsersService usersService) {
+        this.jwtAuthenticationFilter = new JwtAuthenticationFilter (
+                new JwtAuthenticationManager(jwtService, usersService)
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().disable().csrf().disable()
+                .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/about").permitAll()
+                .requestMatchers(HttpMethod.POST, "/user", "/user/login").permitAll()
+                .requestMatchers("/*/**").authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, AnonymousAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+}
